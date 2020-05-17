6ee356081b398208227bae04c90f01c4bf50e99e
Ygor Neves <contato@ygorneves.com>
2020-05-17 06:48:29 -0300

    Initial commit

---
+++ dist/precache-manifest.612bcba4424ade959a08792f66bb3c9f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "496138e807aa67477212",
+    "url": "/css/app.9c164754.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "5fa287555de9b57991b26d614d0ff610",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27187b6304b7707842e8",
+    "url": "/js/about.4736f0ba.js"
+  },
+  {
+    "revision": "496138e807aa67477212",
+    "url": "/js/app.13b5ab26.js"
+  },
+  {
+    "revision": "f72ec43bfcedfa8652be",
+    "url": "/js/chunk-vendors.72b156ed.js"
+  },
+  {
+    "revision": "fc1778b49c7d2c4a2121e56e7760aa1a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
