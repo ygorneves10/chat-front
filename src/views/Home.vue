cb58f8898e696b855067aea91707d04eed6129bd
Ygor Neves <contato@ygorneves.com>
2020-05-17 07:47:13 -0300

    Simple chat with userId

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,51 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <form @submit.prevent="sendMessage">
+      <input type="text" v-model="text" />
+      <button type="submit">Enviar</button>
+    </form>
+    <p v-for="chat in messages" :key="chat.message">
+      <strong>{{chat.user}}</strong>
+      : {{chat.message}}
+    </p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import io from "socket.io-client";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  name: "Home",
+  data() {
+    return {
+      text: "",
+      messages: [],
+      socket: null,
+      userId: null
+    };
+  },
+  mounted() {
+    this.socket = io("https://mighty-spire-83413.herokuapp.com");
+
+    this.socket.on("connect", () => {
+      this.userId = this.socket.id;
+      this.messages.push({
+        user: "Bot",
+        message: `You're connected with id: ${this.userId}`
+      });
+    });
+    this.socket.on("received", message => {
+      this.messages.push(message);
+    });
+  },
+  methods: {
+    sendMessage() {
+      this.socket.emit("message", {
+        user: this.userId,
+        message: this.text
+      });
+      this.text = "";
+    }
   }
-}
+};
 </script>
